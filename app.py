e964f3120ca1f52737a1beaa116537dc86e1de57
Goutham vaishnav <Gouthamvaishnav11>
2025-05-02 18:52:09 +0530

    task manager commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Create the object of the Flask class
 app = Flask(__name__)
